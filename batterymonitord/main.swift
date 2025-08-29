5e2e47a9b60835970a5851b24de1710d92f6ec35
Matthew MAsters <mattdmasters@gmail.com>
2025-08-29 13:51:36 +0800

    rewrite to handler bar server 2 way daemon connection

--- batterymonitord/main.swift
+++ batterymonitord/main.swift
@@ -1,115 +1,175 @@
 import Foundation
 import IOKit.ps
 
-func postJSON(_ json: [String: Any], _ target: String) {
-    guard let url = URL(string: target) else { return }
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.httpBody = try? JSONSerialization.data(withJSONObject: json)
+class WebSocketDaemonClient {
+    private var url: String
+    private var ws: URLSessionWebSocketTask?
+    private var onRequestData: () -> Void
 
-    let task = URLSession.shared.dataTask(with: request) { _, _, error in
-        if let error = error {
-            print("POST error: \(error)")
+    init(_ url: String, _ handler: @escaping () -> Void) {
+        onRequestData = handler
+        self.url = url
+        connect(withURL: self.url)
+    }
+
+    func connect(withURL url: String) {
+        guard let url = URL(string: url) else {
+            print("Invalid url client not initialized")
+            return
+        }
+
+        ws = URLSession(configuration: .default).webSocketTask(with: url)
+
+        if let ws = ws {
+            ws.resume()
+            register()
         }
     }
-    task.resume()
+
+    func register() {
+        guard let ws = ws else {
+            print("couldn't register, Websocket task not initialized")
+            return
+        }
+
+        let data: [String: Any] = [
+            "type": "daemon",
+            "name": "BATTERY_DAEMON",
+        ]
+
+        guard let json = try? JSONSerialization.data(withJSONObject: data) else {
+            print("failed to create registration payload")
+            return
+        }
+
+        guard let payload = String(data: json, encoding: .utf8) else {
+            print("failed to create registration payload")
+            return
+        }
+
+        ws.send(URLSessionWebSocketTask.Message.string(payload)) { [self] error in
+            if let error = error {
+                print("error sending registration message \(error)")
+                return
+            }
+            onRequestData()
+            receive()
+        }
+    }
+
+    func send(data: [String: Any]) {
+        guard let ws = ws else {
+            print("couldn't send, Websocket task not initialized")
+            return
+        }
+
+        guard let json = try? JSONSerialization.data(withJSONObject: data) else {
+            print("couldn't serialize message")
+            return
+        }
+
+        guard let payload = String(data: json, encoding: .utf8) else {
+            print("couldn't serialize message")
+            return
+        }
+
+        ws.send(URLSessionWebSocketTask.Message.string(payload)) { error in
+            if let error = error {
+                print("error sending message \(error)")
+            }
+        }
+    }
+
+    func receive() {
+        guard let ws = ws else {
+            print("couldn't receive, Websocket task not initialized")
+            return
+        }
+
+        ws.receive { [self] result in
+            switch result {
+            case .success:
+                onRequestData()
+                receive()
+                break
+            case .failure:
+                connect(withURL: url)
+                break
+            }
+        }
+    }
+
 }
 
-func getBatteryInfo() -> [String: Any]? {
-    guard let snapshot = IOPSCopyPowerSourcesInfo()?.takeRetainedValue(),
-        let sources: NSArray = IOPSCopyPowerSourcesList(snapshot)?.takeRetainedValue()
-    else {
-        return nil
+class BatteryMonitorWebSocketManager {
+    private var ws: WebSocketDaemonClient?
+
+    init() {
+        ws = WebSocketDaemonClient("ws://localhost:3000/listen") { [self] in
+            sendBatteryInfo()
+        }
     }
 
-    for ps in sources {
-        guard
-            let info = IOPSGetPowerSourceDescription(snapshot, ps as CFTypeRef)?
-                .takeUnretainedValue() as? [String: Any]
-        else {
-            continue
+    func sendBatteryInfo() {
+        guard let ws = ws else {
+            return
         }
 
-        if let current = info[kIOPSCurrentCapacityKey] as? Int,
-            let max = info[kIOPSMaxCapacityKey] as? Int,
-            let state = info[kIOPSPowerSourceStateKey] as? String,
-            let isChargingStatus = info[kIOPSIsChargingKey] as? Bool
-        {
+        guard let info = getBatteryInfo() else {
+            return
+        }
 
-            let percent = Int(Double(current) / Double(max) * 100.0)
+        ws.send(data: ["type": "UPDATE_BATTERY", "data": info])
+    }
 
-            var powerSource = "Unknown"
-            if state == kIOPSACPowerValue {
-                powerSource = "AC"
-            } else if state == kIOPSBatteryPowerValue {
-                powerSource = "Battery"
-            } else if state == kIOPSOffLineValue {
-                powerSource = "Offline"
+    func getBatteryInfo() -> [String: Any]? {
+        guard let snapshot = IOPSCopyPowerSourcesInfo()?.takeRetainedValue(),
+            let sources: NSArray = IOPSCopyPowerSourcesList(snapshot)?.takeRetainedValue()
+        else {
+            return nil
+        }
+
+        for ps in sources {
+            guard
+                let info = IOPSGetPowerSourceDescription(snapshot, ps as CFTypeRef)?
+                    .takeUnretainedValue() as? [String: Any]
+            else {
+                continue
             }
 
-            return [
-                "percentage": percent,
-                "source": powerSource,
-                "charging": isChargingStatus,
-            ]
+            if let current = info[kIOPSCurrentCapacityKey] as? Int,
+                let max = info[kIOPSMaxCapacityKey] as? Int,
+                let state = info[kIOPSPowerSourceStateKey] as? String,
+                let isChargingStatus = info[kIOPSIsChargingKey] as? Bool
+            {
+
+                let percent = Int(Double(current) / Double(max) * 100.0)
+
+                var powerSource = "Unknown"
+                if state == kIOPSACPowerValue {
+                    powerSource = "AC"
+                } else if state == kIOPSBatteryPowerValue {
+                    powerSource = "Battery"
+                } else if state == kIOPSOffLineValue {
+                    powerSource = "Offline"
+                }
+
+                return [
+                    "percentage": percent,
+                    "source": powerSource,
+                    "charging": isChargingStatus,
+                ]
+            }
         }
-    }
-    return nil
-}
-
-func powerSourceChanged(_ context: UnsafeMutableRawPointer?) {
-    if let info = getBatteryInfo() {
-        print("Change detected: \(info)")
-
-        if let target = context?.assumingMemoryBound(to: CChar.self) {
-            postJSON(info, String(cString: target))
-        }
-
+        return nil
     }
 }
 
-let callback: IOPowerSourceCallbackType = { context in
-    powerSourceChanged(context)
+let ws = BatteryMonitorWebSocketManager()
+
+if let runLoopSource = IOPSNotificationCreateRunLoopSource({ _ in ws.sendBatteryInfo() }, nil)?
+    .takeUnretainedValue()
+{
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .defaultMode)
+    CFRunLoopRun()
 }
-
-if CommandLine.arguments.last == "--get" || CommandLine.arguments.last == "-g" {
-    if let info = getBatteryInfo() {
-        let json = try JSONSerialization.data(withJSONObject: info)
-        print(String(data: json, encoding: .utf8) ?? "{}")
-        exit(0)
-    }
-    exit(1)
-}
-
-let args = CommandLine.arguments
-
-if let index = (args.lastIndex(of: "-d") ?? args.lastIndex(of: "--daemonize")) {
-
-    let target = CommandLine.arguments[index + 1].utf8CString
-    let context = UnsafeMutableRawPointer.allocate(
-        byteCount: target.count, alignment: MemoryLayout<UInt8>.alignment)
-
-    defer { context.deallocate() }
-
-    target.withUnsafeBytes { (buffer: UnsafeRawBufferPointer) in
-        context.copyMemory(from: buffer.baseAddress!, byteCount: target.count)
-    }
-    if let runLoopSource = IOPSNotificationCreateRunLoopSource(callback, context)?
-        .takeRetainedValue()
-    {
-        CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .defaultMode)
-        powerSourceChanged(context)
-        CFRunLoopRun()
-    } else {
-        print("Failed to create power source run loop source")
-    }
-}
-
-print(
-    """
-    No valid option provided
-    Options:
-        --get, -g             - Print current battery information as json and exit
-        --daemonize, -d [url] - Daemonize, battery status changes will be posted to the provided url
-    """)
